f3f3636b707c13ae4d2111410000671f9710cd5b
Benjamin Kuhnert <freddi1808@gmx.de>
2026-03-18 13:11:08 +0100

    1

--- Website mit Blog/pythonfile.py
+++ Website mit Blog/pythonfile.py
@@ -5,28 +5,30 @@ import time
 import subprocess
 wait=1
 
-def main():
-    print("RussischRoulette - A simple game of chance")
-    print("Type 'play' to play the game or 'exit' to quit.")
+
+
+
+print("RussischRoulette - A simple game of chance")
+print("Type 'play' to play the game or 'exit' to quit.")
 if input().lower() == 'play'or'Play':
-        print("Generating Number 1-10")
-        number = random.randint(1, 10)
-        while True(wait<10):
-                time.sleep(0.3)
-                print(random.randint(1,10))
-                wait = wait + 1
-        print("Number decided")
-        if input() == number:
-            print("Congratulations! You win!")
-        else:
+    print("Generating Number 1-10")
+    number = random.randint(1, 10)
+    for i in range(10):
+        time.sleep(0.3)
+        print(random.randint(1,10))
             
-            os.system("shutdown -s")
-            subprocess.run("shutdown", "-s")
-            os.remove("pythonfile.py")
-            subprocess.run("sel", "pythonfile.py")
+    print("Number decided")
+    if input() == number:
+        print("Congratulations! You win!")
+    else:
+        
+        
+        subprocess.run("shutdown", "-s")
+        os.remove("pythonfile.py")
+        subprocess.run("sel", "pythonfile.py")
 
 else:    
     print("Exiting the game. Goodbye!")
-    sys.execute("shutdown -s")
     subprocess.run("sel", "pythonfile.py")
     subprocess.run("shutdown", "-s")
+
